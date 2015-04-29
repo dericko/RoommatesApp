067358d2de638d089eaffb5d5163f7fe3d35cba6
Sam Lobel <samlobel@yahoo.com>
2015-04-29 10:32:32 -0400

    add settings controller

---
+++ RoommatesApp/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  RoommatesApp
+//
+//  Created by Sam Lobel on 4/29/15.
+//  Copyright (c) 2015 DerickSam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
