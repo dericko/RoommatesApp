adbcaa83de24856525e55aef78f7e2335c58280c
Derick Olson <olsonderick@gmail.com>
2015-04-29 03:04:06 -0400

    Added search group

---
+++ RoommatesApp/SearchGroupViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchGroupViewController.h
+//  RoommatesApp
+//
+//  Created by Derick Olson on 4/29/15.
+//  Copyright (c) 2015 DerickSam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchGroupViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
